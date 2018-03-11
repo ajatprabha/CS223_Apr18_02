aa7ea8e62f8ad5fcef73d9329eaed86611dcc498
Ajat Prabha <ajat.prabha.leo@gmail.com>
2018-03-12 01:32:55 +0530

    add init class

---
+++ src/init.h
@@ -0,0 +1,14 @@
+//
+// Created by ajatprabha on 12/3/18.
+//
+
+#ifndef CLASSROOMBOOKINGSYSTEM_INIT_H
+#define CLASSROOMBOOKINGSYSTEM_INIT_H
+
+
+class init {
+
+};
+
+
+#endif //CLASSROOMBOOKINGSYSTEM_INIT_H
