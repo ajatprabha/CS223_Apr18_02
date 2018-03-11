bac9bab9cde8dbe03d7deaad022ba11e985a9a0e
Ajat Prabha <ajat.prabha.leo@gmail.com>
2018-03-11 20:27:45 +0530

    add admin/models class

---
+++ src/admin/models.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ajatprabha on 11/3/18.
+//
+
+#include "models.h"
---
+++ src/admin/models.h
@@ -0,0 +1,14 @@
+//
+// Created by ajatprabha on 11/3/18.
+//
+
+#ifndef CLASSROOMBOOKINGSYSTEM_MODELS_H
+#define CLASSROOMBOOKINGSYSTEM_MODELS_H
+
+
+class models {
+
+};
+
+
+#endif //CLASSROOMBOOKINGSYSTEM_MODELS_H
