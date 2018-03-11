953c35f9c1432c595429da9d753875875b3e44a4
Ajat Prabha <ajat.prabha.leo@gmail.com>
2018-03-12 01:56:40 +0530

    add View class

---
+++ src/base/views.h
@@ -0,0 +1,14 @@
+//
+// Created by ajatprabha on 12/3/18.
+//
+
+#ifndef CLASSROOMBOOKINGSYSTEM_VIEWS_H
+#define CLASSROOMBOOKINGSYSTEM_VIEWS_H
+
+
+class View {
+
+};
+
+
+#endif //CLASSROOMBOOKINGSYSTEM_VIEWS_H
