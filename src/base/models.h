b0bae4124cdc313da0b7fbf8c1527c0c55064a5a
Ajat Prabha <ajat.prabha.leo@gmail.com>
2018-03-12 01:04:25 +0530

    add templatised Model class

--- src/base/models.h
+++ src/base/models.h
@@ -5,10 +5,55 @@
 #ifndef CLASSROOMBOOKINGSYSTEM_MODELS_H
 #define CLASSROOMBOOKINGSYSTEM_MODELS_H
 
+#include <map>
 
-class models {
+using namespace std;
 
+
+template<class T>
+class Model {
+private:
+    int id;
+    static map<int, T> objectList;
+public:
+    Model();
+
+    static map<int, T> &all();
+
+    virtual T &save();
+
+    virtual bool remove();
 };
+
+template<class T>
+map<int, T> Model<T>::objectList = {};
+
+template<class T>
+Model<T>::Model() {
+    auto it = objectList.end();
+    if (objectList.size() > 0) {
+        id = (--it)->second.id + 1;
+    } else {
+        id = 1;
+    }
+}
+
+template<class T>
+map<int, T> &Model<T>::all() {
+    return Model<T>::objectList;
+}
+
+template<class T>
+T &Model<T>::save() {
+    Model<T>::objectList.insert(pair<int, T>(this->id, dynamic_cast<T &> (*this)));
+    return dynamic_cast<T &> (*this);
+}
+
+template<class T>
+bool Model<T>::remove() {
+    Model<T>::objectList.erase(this->id);
+    return true;
+}
 
 
 #endif //CLASSROOMBOOKINGSYSTEM_MODELS_H
