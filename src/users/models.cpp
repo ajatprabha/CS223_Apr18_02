1fa0c47b75ccd02a8a89039741440fdcae7891f2
Ajat Prabha <ajat.prabha.leo@gmail.com>
2018-03-12 01:25:15 +0530

    add BaseUser abstract class

--- src/users/models.cpp
+++ src/users/models.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "models.h"
+
+string BaseUser::getFullName() {
+    return string(firstName) + string(lastName);
+}
+
+void BaseUser::setPassword(const string &_password) {
+    for (int i = 0; i < _password.size(); ++i) {
+        password[i] = _password[i];
+    }
+    password[_password.size()] = '\0';
+}
+
+bool BaseUser::checkPassword(const string &_password) {
+    return _password == string(password);
+}
--- src/users/models.h
+++ src/users/models.h
@@ -5,9 +5,24 @@
 #ifndef CLASSROOMBOOKINGSYSTEM_USER_MODELS_H
 #define CLASSROOMBOOKINGSYSTEM_USER_MODELS_H
 
+#include "../base/models.h"
 
-class models {
+class BaseUser : public Model<BaseUser> {
+private:
+    char firstName[32];
+    char lastName[64];
+    char password[64];
+    char email[64];
+public:
+    BaseUser() = default;
 
+    virtual bool isAdmin() = 0;
+
+    string getFullName();
+
+    void setPassword(const string &_password);
+
+    bool checkPassword(const string &_password);
 };
 
 
