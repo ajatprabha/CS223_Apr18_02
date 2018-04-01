257456b8f73409c9d66928b468f6346aa68dc0f5
Ajat Prabha <ajat.prabha.leo@gmail.com>
2018-04-01 22:37:37 +0530

    add Room and Slot class

--- src/booking/models.cpp
+++ src/booking/models.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "models.h"
+
+Slot::Slot(const Professor &requestedBy, const Room &room, const string &startTime, const string &endTime,
+           const string &reason, const string &date, int approved) : requestedBy(requestedBy), room(room),
+                                                                         startTime(startTime), endTime(endTime),
+                                                                         reason(reason), date(date),
+                                                                         approved(approved) {}
+
+Room::Room(int roomNumber, int strength, bool audio, bool video) : roomNumber(roomNumber), strength(strength),
+                                                                   audio(audio), video(video) {}
+
+int Room::getRoomNumber() const {
+    return roomNumber;
+}
+
+int Room::getStrength() const {
+    return strength;
+}
+
+bool Room::hasAudio() const {
+    return audio;
+}
+
+bool Room::hasVideo() const {
+    return video;
+}
+
+void Room::setStrength(int strength) {
+    Room::strength = strength;
+}
+
+void Room::setAudio(bool audio) {
+    Room::audio = audio;
+}
+
+void Room::setVideo(bool video) {
+    Room::video = video;
+}
--- src/booking/models.h
+++ src/booking/models.h
@@ -2,13 +2,50 @@
 // Created by ajatprabha on 1/4/18.
 //
 
-#ifndef CLASSROOMBOOKINGSYSTEM_MODELS_H
-#define CLASSROOMBOOKINGSYSTEM_MODELS_H
+#ifndef CLASSROOMBOOKINGSYSTEM_BOOKING_MODELS_H
+#define CLASSROOMBOOKINGSYSTEM_BOOKING_MODELS_H
 
+#include "../users/models.h"
+#include "../base/models.h"
 
-class models {
+class Room : public Model<Room> {
+private:
+    int roomNumber;
+    int strength;
+    bool audio;
+    bool video;
+public:
+    Room(int roomNumber, int strength, bool audio, bool video);
 
+    int getRoomNumber() const;
+
+    int getStrength() const;
+
+    bool hasAudio() const;
+
+    bool hasVideo() const;
+
+    void setStrength(int strength);
+
+    void setAudio(bool audio);
+
+    void setVideo(bool video);
 };
 
 
-#endif //CLASSROOMBOOKINGSYSTEM_MODELS_H
+class Slot : public Model<Slot> {
+private:
+    Professor requestedBy;
+    Room room;
+    string startTime;
+    string endTime;
+    string reason;
+    string date;
+    int approved = 0;
+public:
+    Slot(const Professor &requestedBy, const Room &room, const string &startTime, const string &endTime,
+         const string &reason, const string &date, int approved = 0);
+};
+
+
+#endif //CLASSROOMBOOKINGSYSTEM_BOOKING_MODELS_H
