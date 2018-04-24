e2f70cb19263a1db6a6b30adc8d7077107261043
Ajat Prabha <ajat.prabha.leo@gmail.com>
2018-04-24 15:32:10 +0530

    update UserDeleteView

--- src/users/views/UserDeleteView.cpp
+++ src/users/views/UserDeleteView.cpp
@@ -3,22 +3,22 @@
 //
 
 #include <init/Application.h>
+#include <base/utils/Input.h>
 #include "UserDeleteView.h"
 
 void UserDeleteView::display() {
-    cout << "Enter the email of user to delete:\n";
+    cout << "Enter the email of user to delete: ";
     string email;
-    cin >> email;
+    email = Input::getEmail();
     BaseUser *user = BaseUser::findByEmail(email);
     if (!user) {
         cout << "No user with this email exists.\n";
-    } else {
-        if (Application::getInstance().getCurrentUser()->getEmail() == email)
-            cout << "You cannot delete yourself!" << endl;
-        else {
-            context.requestObjectId = user->getId();
-            DeleteView::display();
-        }
+    }
+    else if (Application::getInstance().getCurrentUser()->getEmail() == email)
+        cout << "You cannot delete yourself!" << endl;
+    else {
+        context.requestObjectId = user->getId();
+        DeleteView::display();
     }
     response->view = Controller::getInstance().getView("admin-panel");
 }
