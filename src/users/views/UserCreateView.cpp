8cac1c4db770a12b31454668a5368d1da83f4589
Ajat Prabha <ajat.prabha.leo@gmail.com>
2018-04-24 12:44:15 +0530

    update UserCreateView

--- src/users/views/UserCreateView.cpp
+++ src/users/views/UserCreateView.cpp
@@ -4,17 +4,24 @@
 
 #include <admin/forms/UserCreateUpdateForm.h>
 #include <base/controller.h>
+#include <base/utils/Input.h>
 #include "UserCreateView.h"
 
 void UserCreateView::display() {
     cout << "To create a new user fill in the details asked below: \n";
     string firstName, lastName, email, password;
-    char adminStatus;
-    cout << "Enter first name, last name, email and password respectively.\n";
-    cin >> firstName >> lastName >> email >> password;
-    cout << "Should this user be an admin? (y/n)\n";
-    cin >> adminStatus;
-    form = new UserCreateUpdateForm(firstName, lastName, email, password, adminStatus == 'y');
+    bool adminStatus;
+    cout << "First name: ";
+    firstName = Input::getString();
+    cout << "Last name: ";
+    lastName = Input::getString();
+    cout << "Email: ";
+    email = Input::getEmail();
+    cout << "Password: ";
+    password = Input::getPassword();
+    cout << "Should this user be an admin? (y/n): ";
+    adminStatus = (Input::getChar() == 'y');
+    form = new UserCreateUpdateForm(firstName, lastName, email, password, adminStatus);
     if (form->isValid()) {
         BaseUser user = form->save();
         cout << "New user, " + user.getFullName() + " saved successfully.\n";
